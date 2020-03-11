7081ff46054a0b6cf832d3eb90dff66c5bdbe43f
David-Project09 <54827654+David-Project09@users.noreply.github.com>
2020-03-11 08:20:58 -0600

    Add files via upload

---
+++ tarea1p1.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <iostream>
+#include <conio.h>
+
+using std::cin;
+using std::cout;
+
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+
+	srand(time(NULL));
+	int secretnumber = rand() % 10;
+	int x = 0;
+	cout << "select number";
+	cin >> x;
+
+	while (int > 10 = is higher; int < 10 = is lower; int = 10 is the same;)
+
+	{
+	if (x > secretnumber)
+		cout << "\n secretnumber is higher:" << x;
+	else
+		if (x > secretnumber)
+			cout << "\n secretnumber is lower" << x;
+
+		else
+			if (x = secretnumber)
+				cout << "\n the number is the same... chau" << x;
+
+	}
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
+
+
